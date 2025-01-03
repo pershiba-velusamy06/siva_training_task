6259f5cfbc6a96ecb96ea10e3721468f88aa4a27
Siva-Elred <siva@elred.io>
2025-01-03 18:37:43 +0530

    practice js scripts 03-01-25

---
+++ test.js
@@ -0,0 +1,78 @@
+// Write a Function to swap 2 numbers using javascript.
+
+var    a = 10;
+var     b = 20;
+
+let temp = a;
+a = b;
+b = temp;
+
+document.write("A="+ a);
+document.write("B="+b);
+
+//Check whether the string passed is palindrome using javascript
+
+function palindrome(text)
+{
+   const checklowercase = text.toLowerCase();
+
+    const reversetect = checklowercase.split('').reverse().join('');
+
+    if (checklowercase===reversetect)
+{
+
+    console.log(`${text} is Palindrome`);
+}
+
+else
+
+{
+    console.log(`${text} is not palindrom`);
+}
+
+
+
+}
+
+palindrome("siva")
+
+
+// Write a function to reverse a string using javascript.
+function siva (text)
+
+{
+    const reversetext = text.split('').reverse().join("");
+    document.write(`${reversetext} is the reversed string`)
+}
+
+
+siva("elred")
+
+//Write a program to print numbers from 1 to 30. For multiples of 3, print “Fizz” instead of the number, and for multiples of 5, print “Buzz”. For multiples of both 3 and 5, print “FizzBuzz”.
+function siva()
+
+{
+    for(let i=1;i<=30;i++){
+        if (i%3 ==0)
+            {
+             document.write("fizz");
+            }
+
+            else if (i%5==0)
+            {
+                document.write("buzz")
+            }
+            else if(i%3==0 && i%5==0){
+                document.write("fizzbuzz");
+            }
+            else{
+
+                document.write(i);
+            }
+
+    }
+
+   
+}
+
+siva()
